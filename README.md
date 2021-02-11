d723cccf4e65b5a17412e65ee940463f430011cf
parsa shahmaleki <parsampsh@gmail.com>
2021-02-11 16:24:07 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# TChess : Play the Chess in the terminal
+**TChess** is a simple chess game in terminal.
+With this program, you can play chess in terminal.
+
+This software is licensed under [MIT](/LICENSE).
+
+## Get Started
+To run a game:
+
+```bash
+$ tchess
+```
+
+Tchess can save your game in a file, then you can continue that game later.
+If you run the above command, game will be saved in `game.tchess` file in current working directory.
+
+But you can customize the file:
+
+```bash
+$ tchess my-game.bin
+```
+
+Also if you played a game and closed that and now you want to continue that game, only give file name as argument:
+
+```bash
+$ tchess my-old-game.bin
+```
+
+(The `.bin` extension is a example, you can name it whatever you want).
+
+## Basics
---
+++ tchess.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+
+import pickle
+import sys
+import os
+
+""" Play the Chess in the terminal """
+
+class Piece:
+    """ Each piece in the chess board """
+
+    PAWN = 'pawn'
+    KING = 'king'
+    QUEEN = 'queen'
+    KNIGHT = 'knight'
+    BISHOP = 'bishop'
+    ROCK = 'rock'
+
+    def __init__(self, name: str, color: str, id: int, game):
+        self.name = name
+        self.game = game
+        self.color = color
+        self.id = id
+
+    def __str__(self):
+        return self.color + '-' + self.name
+
+class Game:
+    """ The running game handler """
+
+    ROW_SEPARATOR = '|--------------|--------------|--------------|--------------|--------------|--------------|--------------|--------------|\n'
+
+    def __init__(self):
+        self.turn = 'white'
+        self.logs = []
+
+        # initialize the board
+        self.board = []
+        for i in range(8):
+            self.board.append([])
+            for j in range(8):
+                # handle default pieces location
+                if i == 1 or i == 6:
+                    self.board[-1].append(
+                        Piece(
+                            name=Piece.PAWN,
+                            color=('white' if i == 1 else 'black'),
+                            id=int(str(i) + str(j)),
+                            game=self
+                        )
+                    )
+                elif i in (0, 7):
+                    name = Piece.PAWN
+                    if j in (0, 7):
+                        name = Piece.ROCK
+                    elif j in (0, 3):
+                        name = Piece.KING
+                    elif j in (3 ,7):
+                        name = Piece.QUEEN
+                    elif j in (0, 4):
+                        name = Piece.QUEEN
+                    elif j in (4, 7):
+                        name = Piece.KING
+                    elif j in (2, 5):
+                        name = Piece.KNIGHT
+                    elif j in (1, 6):
+                        name = Piece.BISHOP
+                    self.board[-1].append(
+                        Piece(
+                            name=name,
+                            color=('white' if i == 0 else 'black'),
+                            id=int(str(i) + str(j)),
+                            game=self
+                        )
+                    )
+                else:
+                    self.board[-1].append(None)
+
+    def change_turn(self):
+        """ Changes the turn.
+
+        If currently is white turn, set turn to black and reverse
+        """
+        self.turn = 'black' if self.turn == 'white' else 'white'
+
+    def run_command(self, cmd: str):
+        """ Gets a command as string and runs that on the game """
+        # TODO : run the command
+
+        # add command to the log
+        self.logs.append(cmd)
+
+        # change the turn
+        self.change_turn()
+
+    def render(self) -> str:
+        """ Renders the board to show in the terminal """
+        output = ''
+        for row in list(reversed(self.board)):
+            output += self.ROW_SEPARATOR
+            for column in row:
+                if column is None:
+                    column_str = ' ' * 13
+                else:
+                    column_str = str(column)
+                output += '| ' + column_str + (' ' * (13-len(column_str)))
+            output += '|\n'
+        output += self.ROW_SEPARATOR
+        return output
+
+def run(args: list):
+    """ The main cli entry point """
+    game_file_name = 'game.tchess'
+
+    # parse the arguments
+    options = [arg for arg in args if arg.startswith('-')]
+    arguments = [arg for arg in args if not arg.startswith('-')]
+
+    if len(arguments) > 0:
+        game_file_name = arguments[0]
+
+    if os.path.isfile(game_file_name):
+        # if file exists, load the game from that
+        # (means user wants to open a saved game)
+        tmp_f = open(game_file_name, 'rb')
+        game = pickle.load(tmp_f)
+        tmp_f.close()
+    else:
+        game = Game()
+
+    while True:
+        # render the game board on the terminal
+        print('\033[H', end='')
+        title = '*** Welcome to the TChess! ***'
+        print(title, end='')
+        print(' ' * (len(Game.ROW_SEPARATOR) - len(title)))
+        print()
+        print(game.render())
+
+        # get command from user and run it
+        command = input(game.turn + ' Turn >>> ').strip().lower()
+
+        # check the empty command
+        if command == '':
+            continue
+
+        # check the exit command
+        if command in ['exit', 'quit', 'q']:
+            game_file_name = os.path.abspath(game_file_name)
+            print('Your game was saved in file `' + game_file_name + '`.')
+            print('To continue this game again, run `' + sys.argv[0] + ' ' + repr(game_file_name) + '`.')
+            print('Good bye!')
+            sys.exit()
+
+        # run the command on the game to make effects
+        game.run_command(command)
+
+        # save the game
+        # open a file
+        # this file is used to save the game state
+        # after any command on the game, game will be re-write on this file
+        game_file = open(game_file_name, 'wb')
+        pickle.dump(game, game_file)
+        game_file.close()
+
+if __name__ == '__main__':
+    run(sys.argv[1:])
