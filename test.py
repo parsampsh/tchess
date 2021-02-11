8d828df23cf0fccbd8e624fd3c317d99ac241677
parsa shahmaleki <parsampsh@gmail.com>
2021-02-11 21:13:10 +0330

    update test

--- test.py
+++ test.py
@@ -4,6 +4,13 @@
 
 from tchess.tchess import Game, Piece
 
+def str_contains_all(string, items):
+    """ Gets a string and a list of string and checks all of list items are in string """
+    for item in items:
+        if not item in string:
+            return False
+    return True
+
 def test_default_state_is_valid():
     """ Default state of chess board is valid """
     game = Game()
@@ -51,7 +58,18 @@ def test_command_runner_works():
     game = Game()
 
     assert 'invalid' in game.run_command('fdfgdd').lower()
-    #assert game.run_command('mov ') in result.lower()
+    assert str_contains_all(game.run_command('mv 4.3 to 6-3').lower(), ['error', 'source', 'empty'])
+    assert game.run_command('mv hi to 43543').lower().startswith('invalid')
+    assert str_contains_all(game.run_command('mv 7.2 to 5.3').lower(), ['error', 'turn'])
+    assert str_contains_all(game.run_command('mv 2.3 to 2.4').lower(), ['error', 'kill', 'self'])
+
+    assert game.board[1][0] is not None
+    assert game.board[3][0] is None
+    assert str_contains_all(game.run_command('mv 2.1 to 4.1').lower(), ['moved', '2.1', '4.1'])
+    assert game.board[1][0] is None
+    assert game.board[3][0] is not None
+    assert game.board[3][0].name == Piece.PAWN
+
 
 def test_game_file_system_works():
     """ Game file system working correct """
