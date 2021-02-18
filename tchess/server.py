5eb9b892acd87dbdfa4a679355510f390eb8a3d5
parsa shahmaleki <parsampsh@gmail.com>
2021-02-18 17:11:24 +0330

    return turn in /render

--- tchess/server.py
+++ tchess/server.py
@@ -56,8 +56,8 @@ def serve(game_object, host='0.0.0.0', port=8799):
 
         try:
             if request.args['session'] == CURRENT_SESSION:
-                # render the game
-                return game_object.render()
+                # render the game and turn
+                return game.turn + '\n' game_object.render()
             else:
                 raise
         except:
